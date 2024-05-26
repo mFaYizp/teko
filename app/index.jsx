30428645710e56d189ad27c64b61d30023053cbb
mFaYizp <fayizfaz78@gmail.com>
2024-05-26 22:11:16 +0530

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import {Text, View } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
+
+const index = () => {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <Text className="text-3xl font-pblack">Teko</Text>
+      <StatusBar style="auto" />
+      <Link className="text-red-500 mt-10" href={"/profile"}>Go to Profile</Link>
+    </View>
+  );
+};
+
+export default index;
